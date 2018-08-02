8796a651d3a4ae2132f61cb31d5c09ae0d89c076
Kurt Zenisek <KZeni1@gmail.com>
2018-08-02 15:29:44 -0500

    Switched to Extra version for video/iframe support.

--- assets/js/jquery.lazyloadxt.js
+++ assets/js/jquery.lazyloadxt.js
@@ -310,3 +310,34 @@
     });
 
 })(window.jQuery || window.Zepto || window.$, window, document);
+
+
+(function ($) {
+    var options = $.lazyLoadXT;
+
+    options.selector += ',video,iframe[data-src]';
+    options.videoPoster = 'data-poster';
+
+    $(document).on('lazyshow', 'video', function (e, $el) {
+        var srcAttr = $el.lazyLoadXT.srcAttr,
+            isFuncSrcAttr = $.isFunction(srcAttr),
+            changed = false;
+
+        $el.attr('poster', $el.attr(options.videoPoster));
+        $el.children('source,track')
+            .each(function (index, el) {
+                var $child = $(el),
+                    src = isFuncSrcAttr ? srcAttr($child) : $child.attr(srcAttr);
+                if (src) {
+                    $child.attr('src', src);
+                    changed = true;
+                }
+            });
+
+        // reload video
+        if (changed) {
+            this.load();
+        }
+    });
+
+})(window.jQuery || window.Zepto || window.$);
